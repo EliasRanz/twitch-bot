484cf238185f1c6e92bc173ce7c6ed236b26d649
Elias Ranz-Schleifer <Elias Ranz-Schleifer>
2017-03-23 22:18:16 -0400

    Removing settings.py from repo.

--- settings.py
+++
@@ -1,8 +0,0 @@
-#!C:\Python27\python.exe
-HOST = "irc.twitch.tv"
-PORT = 6667
-BOT_USERNAME = "Xxplodibot"
-CHANNEL = "xxplosions"
-PASSWORD = "oauth:hmo1uyyh7oojxc251tumerj2gv1sp1"
-TWITCH_CLIENT_ID = "20aijit7xodpo8qumioq6e8vs5j6m3"
-
